5693675bd215fdfa7c662d07a33316b0b1c08183
tetrisgirl89 <146213633+tetrisgirl89@users.noreply.github.com>
2023-12-02 03:14:18 +0000

    restart save

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
-
+  get("/directors", { :controller => "directors", :action => "directorsindex" })
+  get("/directors/:the_id", { :controller => "directors", :action => "show" })
+  get("/movies", { :controller => "movie", :action => "movieaction" })
+  get("/actors", { :controller => "actor", :action => "actoraction" })
 end
